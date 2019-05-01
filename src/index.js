12892299aa8b1a8e1da179fbede4dabba87250d3
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-05-01 14:07:48 -0300

    lifecycle control

--- src/index.js
+++ src/index.js
@@ -6,10 +6,37 @@ import Todo from './components/Todo'
 export default class App extends Component {
   state = {
     usuario: 'Rafael',
+    counter: 0,
     todos: [
       { id: 0, text: 'Fazer café' },
       { id: 1, text: 'Estudar o GONative' }
     ]
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ text: 'Hello world' });
+    }, 3000)
+  }
+
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return { text: nextProps.text }
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter < 5;
+  }
+
+  // componentDidUpdate (prevProps, prevState)  {
+
+  // }
+
+  // componentWillMount() {
+
+  // }
+
+  handleAddCounter = () => {
+    this.setState({ counter: this.state.counter + 1 })
   }
 
   addTodo = () => {
@@ -20,13 +47,16 @@ export default class App extends Component {
   }
 
   render() {
-    const { todos, usuario } = this.state
+    const { todos, usuario, text, counter } = this.state
 
     return (
       <View style={styles.container}>
+        <Text>{text}</Text>
         <Text>{usuario}</Text>
         {todos.map(todo => <Todo key={todo.id} title={todo.text} />)}
         <Button title="Add todo" onPress={this.addTodo} />
+        <Text>{counter}</Text>
+        <Button title="Counter" onPress={this.handleAddCounter} />
       </View>
     );
   }
